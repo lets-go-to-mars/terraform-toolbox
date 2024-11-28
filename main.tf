a967592e9af0a58570fe52755667cd909b0ef338
localfahad <localfahad@interac.ca>
2024-11-28 12:14:44 -0500

    Added aws provider and credentials

--- main.tf
+++ main.tf
@@ -13,7 +13,11 @@ terraform {
 
 #     prefix = "${var.resource_prefix}-${var.environment}"
 # }
-
+provider "aws" {
+  region     = "us-west-2"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+}
 module "vpc" {
   source = "./aws/vpc"
 }
